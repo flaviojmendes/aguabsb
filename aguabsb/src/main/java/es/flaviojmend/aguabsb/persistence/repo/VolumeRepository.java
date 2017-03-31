5c028ca79ad3d9d25589cd96297eeca2d4a0f285
Flávio Mendes <flaviojmendes@gmail.com>
2017-03-31 07:35:03 -0300

    first commit

---
+++ aguabsb/src/main/java/es/flaviojmend/aguabsb/persistence/repo/VolumeRepository.java
@@ -0,0 +1,12 @@
+package es.flaviojmend.aguabsb.persistence.repo;
+
+import es.flaviojmend.aguabsb.persistence.entity.Volume;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface VolumeRepository extends CrudRepository<Volume, String> {
+
+
+
+}
+
